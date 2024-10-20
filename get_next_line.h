2ad435ff8f1222ebc53acccb6863e9ec6a4629c3
supermarius666 <mmm69759@gmail.com>
2024-10-20 18:56:47 +0200

    created project

---
+++ get_next_line.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mdumitru <mdumitru@student.42roma.it>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/20 18:53:43 by mdumitru          #+#    #+#             */
+/*   Updated: 2024/10/20 18:56:14 by mdumitru         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE
+# define GET_NEXT_LINE
+
+# include <stdlib.h>
+# include <unistd.h>
+
+char    *get_next_line(int fd);
+#endif
